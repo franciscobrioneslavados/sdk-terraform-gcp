8144032aa165abf7ed3bbd0521c9fa5b55be57b1
Francisco Briones <franciscobrioneslavados@gmail.com>
2021-12-13 11:08:38 -0300

    Delete setup-environment.sh

--- setup-environment.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-export PROJECT_ID=stately-avatar-334702
-export REGION=us-central1
-export ZONE=us-central1-c
-export CLUSTER_NAME=learnk8s-cluster
-export DOCKER_APP_NAME=acl-tae-pvs
-export SQL_INSTANCE_NAME=test-instance
